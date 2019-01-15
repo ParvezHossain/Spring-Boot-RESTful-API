2bcb7298e4acee9dcddc61ecf8798b7123ce3960
ParvezHossain <lazyengineer.java@gmail.com>
2019-01-15 16:55:06 +0600

    Initial Project Structure

---
+++ src/main/java/com/lazyengineer/SpringBootResTfulApiApplication.java
@@ -0,0 +1,14 @@
+package com.lazyengineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResTfulApiApplication.class, args);
+	}
+
+}
+
